329b5407cdb90bf13d2190567cbc18257a55c886
Abenezer Nuro <abenuro@gmail.com>
2022-09-06 11:43:25 -0400

    css

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -9,16 +9,53 @@ function Landing() {
    const {meeting,  user} = useContext(MeetingContext);
 //    
     return (
-        <>
-            <h2>Choose a Chat Room</h2>
-            <ul className="chat-room-list">
-                {meeting.map((meeting) => (
-                    <li key={meeting.id}>
-                        <Link to={`/room/${meeting.id}`}  >{meeting.id}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
+      <>
+        <h2>Choose a Chat Room</h2>
+        <ul
+          className="container max-width: 1024px;
+"
+        >
+          {meeting.map((meeting) => (
+            <div
+              className=" max-width: 1024px;
+"
+               key={meeting.id}
+            >
+              <Link to={`/room/${meeting.id}`}>
+                <div class=" p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {meeting.id}
+                    </h5>
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {meeting.joindList.length} people are joind
+                  </p>
+                  <a
+                    href="#"
+                    class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Enter{" "}
+                    <svg
+                      aria-hidden="true"
+                      class="ml-2 -mr-1 w-4 h-4"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </ul>
+      </>
     );
 }
 
